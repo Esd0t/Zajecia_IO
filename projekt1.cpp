ccef5967e90c59ee808485a892e9ca4fa1f5df9b
Emilia Szotowicz <126385717+Esd0t@users.noreply.github.com>
2026-03-31 09:24:03 +0200

    Dodanie funkcji czyPierwsza do menu

--- projekt1.cpp
+++ projekt1.cpp
@@ -25,7 +25,18 @@ int main() {
         cout << "MENU" << endl;
         cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
         cout << "0. Wyjscie" << endl;
+	cout << "1. Czy liczba jest pierwsza" << endl;
         cin >> wyjscie;
+	if (wyjscie == 1){
+		cout << "Podaj liczbę do sprawdzenia: ";
+		int liczba;
+		cin >> liczba;
+		if(czyPierwsza(liczba)){
+			cout << "Liczba pierwsza" << endl;
+		}else{
+			cout << "Nie jest to liczba pierwsza" << endl;
+		}
+	}
     } while(wyjscie != 0);
     return 0;
 }
